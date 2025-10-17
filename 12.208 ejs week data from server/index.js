52ed7df04ece91134a8bcefed0d9a1e9468195c4
Sneha Yadav <164773937+SNH078@users.noreply.github.com>
2025-10-17 22:04:08 +0530

    Add files via upload

---
+++ 12.208 ejs week data from server/index.js
@@ -0,0 +1,37 @@
+//send data from server to client 
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  //if nothing is entered in date ..current day wil get passed automatically 
+  
+  //Test code
+  // weekend:
+  // new Date("June 24, 2023 11:13:00");
+  // weekday:
+  // new Date("June 20, 2023 11:13:00");
+  const day = today.getDay();
+  // console.log(day);
+  
+  //set default 
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
